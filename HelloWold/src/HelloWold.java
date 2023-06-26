0042479cc97141f09af145a33b117926949b6595
Mehri <manizheh.mehri@gmail.com>
2023-06-26 18:32:47 +0200

    Initial commit

--- HelloWold/src/HelloWold.java
+++ HelloWold/src/HelloWold.java
@@ -4,6 +4,7 @@ public class HelloWold {
 	public static void main(String[] args) {
       System.out.println(" Hello World");
       System.out.println("ich habe geschaft");
+      System.out.println("ich habe noch mal Probiert");
 
 	}
 
